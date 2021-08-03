3647cb9bd75c2632c8ee5388277f1dd7b3c33015
rbwo <66784492+rbwo552@users.noreply.github.com>
2021-08-03 20:42:44 +0900

    sw_5105

--- sw/sw_queue/sw_queue06/06.py
+++ sw/sw_queue/sw_queue06/06.py
@@ -1,4 +1,4 @@
-def bfs(row, col) :
+def bfs(row, col) :     #bfs 를 사용하여서 문제를 풀었다.
     dr = [0,0,-1,1]
     dc = [-1,1,0,0]
 
@@ -13,20 +13,20 @@ def bfs(row, col) :
             nc = col + dc[i]
             if 0<= nr < M and 0<= nc < M :
                 if matrix[nr][nc] == 3:
-                    distance[nr][nc] = distance[row][col]
+                    distance[nr][nc] = distance[row][col]       # 탈출점이므로 그전의 값을 대입해줌.
             
                 elif matrix[nr][nc] == 0 and visited[nr][nc] == 0 :
                     visited[nr][nc] = 1
-                    distance[nr][nc] = distance[row][col] + 1
+                    distance[nr][nc] = distance[row][col] + 1       # 거리간의 차이를 넣어줌
                     queue.append((nr,nc))
 
-    return distance[end_row][end_col]
+    return distance[end_row][end_col]   #만약 해당 점이 없다면 0이 이미 들어가있으므로 출구 도달 하는것과는 상관없이 return
                     
 for i in range(1,int(input()) + 1):
     M = int(input())
     count = 0
     matrix = []
-    distance = [[0] * M for _ in range(M)]
+    distance = [[0] * M for _ in range(M)]      #해당 점이랑 출발점사이의 거리를 나타낼 행렬
     visited = [[0 for _ in range(M)] for _ in range(M)]
     for k in range(M):
         array = list(map(int,input().strip()))
