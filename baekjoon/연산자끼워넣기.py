9506be486d328a1d5f183dfdeac5599fcfebfc04
rbwo <66784492+rbwo552@users.noreply.github.com>
2021-08-11 20:11:06 +0900

    boj_14888

--- baekjoon/연산자끼워넣기.py
+++ baekjoon/연산자끼워넣기.py
@@ -1,52 +1,48 @@
-'''
-푸는 중.. 
-'''
+from collections import deque # import 하고 사용안하였다.. 
 
-
-from collections import deque
-
-def func(num,op_idx,i) :
+def func(temp,op_idx,i) : # 연산을 실행할 함수
     if op_idx == 0 :
-        num[i+1] += num[i] 
+        temp += number[i+1] 
 
     elif op_idx == 1 :
-        num[i+1] -= num[i]
+        temp -= number[i+1]
 
     elif op_idx == 2 :
-        num[i+1] *= num[i]
+        temp *= number[i+1]
     
     else :
-        if num[i] < 0:
-            num[i] *= -1
-            num[i+1] //= num[i]
-            num[i+1] *= -1
+        if temp < 0:
+            temp *= -1
+            temp //= number[i+1]
+            temp *= -1
             
         else :
-            num[i+1] //= num[i]
+            temp //= number[i+1]
     
-    return num[i+1]
+    return temp
 
 
-def dfs(num,op_idx,idx) :
+def dfs(temp,op_idx,idx) :
     global max_num
     global min_num
 
-    temp = func(num,op_idx,idx)
+    temp = func(temp,op_idx,idx)
     idx += 1
 
-    if max_num < temp :
-        max_num = temp
-    
-    if min_num > temp :
-        min_num = temp
+    if idx == len(number) - 1 :     # 한번의 연산과정이 전부 끝났다면 max min 값 비교하고 return
 
-    if idx == len(num) :
+        if max_num < temp :
+            max_num = temp
+    
+        if min_num > temp :
+            min_num = temp
+
         return 
 
     for i in range(4):
         if op[i] != 0:
             op[i] -= 1
-            dfs(num,i,idx)
+            dfs(temp,i,idx)
             op[i] += 1
     
 
@@ -58,10 +54,12 @@ sum_num = 0
 max_num = -1000000001
 min_num = 10000000001
 
+temp = number[0]
+
 for i in range(4) :
     if op[i] != 0:
         op[i] -= 1
-        dfs(number,i,0)
+        dfs(temp,i,0)       # dfs 한 뒤에 i를 다시 연산자 리스트를 다시 원상복구해줌
         op[i] += 1
 
 print(max_num)
