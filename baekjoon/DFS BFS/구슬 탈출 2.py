13b31571e33990e49351c305775b339e38dcd928
rbwo <rbwo552@naver.com>
2021-09-14 17:24:29 +0900

    2021_09_14

--- baekjoon/DFS BFS/구슬 탈출 2.py
+++ baekjoon/DFS BFS/구슬 탈출 2.py
@@ -19,88 +19,119 @@ def move(y,x,i) :
   if i < 2 :
     for k in range(M-1) :
       if 0 < nx < M :
-        if board[ny][nx] == '.' or board[ny][nx] == 'B' :
+        if board[ny][nx] == '.' or board[ny][nx] == 'B' or board[ny][nx] == 'R' :
           nx += dx[i]
         elif board[ny][nx] == 'O' :
           return ny,nx
-        else :
+        if board[ny][nx] == '#' :
           nx -= dx[i]
           return ny,nx
     
   else :
     for k in range(N-1) :
       if 0 < ny < N :
-        if board[ny][nx] == '.' or board[ny][nx] == 'B' :
+        if board[ny][nx] == '.' or board[ny][nx] == 'B' or board[ny][nx] == 'R'  :
           ny += dy[i]
         elif board[ny][nx] == 'O' :
           return ny,nx
-        else :
+        if board[ny][nx] == '#' :
           ny -= dy[i]
           return ny,nx
 
   return y,x
 
 def bfs(red_y,red_x,blue_y,blue_x):
+  checklist = dict()
   global count
-  t = 0
-  deq_red = deque()
-  deq_blue = deque()
-  deq_red.append((red_y,red_x))
-  deq_blue.append((blue_y,blue_x))
+  deq = deque()
 
-  while count != 11 :
+  deq.append((red_y,red_x,blue_y,blue_x))
+  checklist[(red_y,red_x,blue_y,blue_x)] = 1
+
+  while deq:
+    temp = []
     count += 1
-    ry,rx = deq_red.popleft()
-    by,bx = deq_blue.popleft()
+    if count > 10 :
+      return -1
+    for i in range(len(deq)) :
+      temp.append(deq.popleft())
+    
+    for ry,rx,by,bx in temp :
+      board[ry][rx], board[red_y][red_x] = board[red_y][red_x], board[ry][rx]
+      board[by][bx], board[blue_y][blue_x] = board[blue_y][blue_x], board[by][bx]
+      chk = 1
+      t = -1
+      
+      if ry == by :
+        if rx < bx :
+          nby, nbx = move(by,bx,1)
+          nry, nrx = move(ry,rx,1)
+          
+          if nbx == nrx :
+            nrx -= dx[1]
 
-    if ry == by :
-      if rx < bx :
-        nby, nbx = move(by,bx,1)
-        nry, nrx = move(ry,rx,1)
-        if check(nry,nrx,nby,nbx) :
-          return count
-        if nbx == nrx :
-          nrx -= dx[1]
-        deq_blue.append((nby,nbx))
-        deq_red.append((nry,nrx))
-        t = 1
-      elif rx > bx :
-        nby, nbx = move(by,bx,0)
-        nry, nrx = move(ry,rx,0)
-        if check(nry,nrx,nby,nbx) :
-          return count
-        if nbx == nrx :
-          nrx -= dx[0]
-        deq_blue.append((nby,nbx))
-        deq_red.append((nry,nrx))
-        t = 0
-    if rx == bx :
-      if ry > by :
-        nby, nbx = move(by,bx,2)
-        nry, nrx = move(ry,rx,2)
-        if check(nry,nrx,nby,nbx) :
-          return count
-        if nby == nry :
-          nry -= dx[2]
-        deq_blue.append((nby,nbx))
-        deq_red.append((nry,nrx))
-        t = 2
-      elif ry < by :
-        nby, nbx = move(by,bx,3)
-        nry, nrx = move(ry,rx,3)
-        if check(nry,nrx,nby,nbx) :
-          return count
-        if nby == nry :
-          nry -= dx[3]
-        deq_blue.append((nby,nbx))
-        deq_red.append((nry,nrx))
-        t = 3
+          if check(nry,nrx,nby,nbx) :
+            return count
 
-    else :
+          if (nry,nrx,nby,nbx) not in checklist and board[nby][nbx] != 'O':
+            deq.append((nry,nrx,nby,nbx))
+            checklist[(nry,nrx,nby,nbx)] = 1
+          
+          t = 1
+
+        elif rx > bx :
+          nby, nbx = move(by,bx,0)
+          nry, nrx = move(ry,rx,0)
+        
+          if nbx == nrx :
+            nrx -= dx[0]
+
+          if check(nry,nrx,nby,nbx) :
+            return count
+
+          if (nry,nrx,nby,nbx) not in checklist and board[nby][nbx] != 'O' :
+            deq.append((nry,nrx,nby,nbx))
+            checklist[(nry,nrx,nby,nbx)] = 1
+
+          t = 0
+
+      elif rx == bx :
+        if ry > by :
+          nby, nbx = move(by,bx,2)
+          nry, nrx = move(ry,rx,2)
+
+          if nby == nry :
+            nry -= dy[2]
+
+          if check(nry,nrx,nby,nbx) :
+            return count
+
+          if (nry,nrx,nby,nbx) not in checklist and board[nby][nbx] != 'O' :
+            deq.append((nry,nrx,nby,nbx))
+            checklist[(nry,nrx,nby,nbx)]= 1
+          t = 2
+        elif ry < by :
+          nby, nbx = move(by,bx,3)
+          nry, nrx = move(ry,rx,3)
+
+          if nby == nry :
+            nry -= dy[3]
+
+          if check(nry,nrx,nby,nbx) :
+            return count
+
+          if (nry,nrx,nby,nbx) not in checklist and board[nby][nbx] != 'O' :
+            deq.append((nry,nrx,nby,nbx))
+            checklist[(nry,nrx,nby,nbx)] = 1
+          t = 3
+
       for i in range(4) :
+        
         if i == t :
           continue
+
         chk = 1
+
         n_ry = ry + dy[i]
         n_rx = rx + dx[i]
 
@@ -109,8 +140,8 @@ def bfs(red_y,red_x,blue_y,blue_x):
 
         if i < 2 :
           for k in range(M-1) :
-            if 0 < n_rx < M :
-              if board[n_ry][n_rx] == '.' :
+            if 0 < n_rx < M   :
+              if board[n_ry][n_rx] == '.' or board[n_ry][n_rx] == 'B' or board[n_ry][n_rx] == 'R' :
                 n_rx += dx[i]
               elif board[n_ry][n_rx] == 'O' :
                 chk = 2
@@ -118,32 +149,38 @@ def bfs(red_y,red_x,blue_y,blue_x):
               if board[n_ry][n_rx] == '#' :
                 n_rx -= dx[i]
                 break 
+            elif 0 == n_rx or M-1 == n_rx :
+              n_rx -= dx[i]
+              break
 
           for k in range(M-1) :
-            if 0 < n_bx < M :
-              if n_bx == n_rx :
+            if 0 < n_bx < M -1 :
+              if n_bx == n_rx and n_by == n_ry and board[n_by][n_bx] != 'O':
                 n_bx -= dx[i]
                 break
-              if board[n_by][n_bx] == '.' or board[n_by][n_bx] == 'R':
+              if board[n_by][n_bx] == '.' or board[n_by][n_bx] == 'R' or board[n_by][n_bx] == 'B' :
                 n_bx += dx[i]
               elif board[n_by][n_bx] == 'O' :
                 chk = 0
                 break
-              if board[n_by][n_bx] == '#':
+              elif board[n_by][n_bx] == '#':
                 n_bx -= dx[i]
                 break
+            elif 0 == n_bx or n_bx == M-1 :
+              n_bx -= dx[i]
+              break
 
-          if chk == 1 :
-            deq_red.append((n_ry,n_rx))
-            deq_blue.append((n_by,n_bx))
+          if chk == 1 and (n_ry,n_rx,n_by,n_bx) not in checklist :
+            deq.append((n_ry,n_rx,n_by,n_bx))
+            checklist[(n_ry,n_rx,n_by,n_bx)] = 1
           
-          if chk == 2 :
+          if check(n_ry,n_rx,n_by,n_bx) :
             return count
 
         else :
           for k in range(N-1) :
             if 0 < n_ry < N :
-              if board[n_ry][n_rx] == '.' :
+              if board[n_ry][n_rx] == '.' or board[n_ry][n_rx] == 'B' or board[n_ry][n_rx] == 'R' :
                 n_ry += dy[i]
               elif board[n_ry][n_rx] == 'O' :
                 chk = 2
@@ -151,27 +188,34 @@ def bfs(red_y,red_x,blue_y,blue_x):
               if board[n_ry][n_rx] == '#' :
                 n_ry -= dy[i]
                 break
+            elif 0 == n_ry or n_ry == N-1 :
+              n_ry -= dy[i]
+              break
 
           for k in range(N-1) :
             if 0 < n_by < N :
-              if n_by == n_ry :
+              if n_by == n_ry and n_bx == n_rx and board[n_by][n_bx] != 'O' :
                 n_by -= dy[i]
                 break
-              if board[n_by][n_bx] == '.' or board[n_by][n_bx] == 'R' :
+              if board[n_by][n_bx] == '.' or board[n_by][n_bx] == 'R' or board[n_by][n_bx] == 'B' :
                 n_by += dy[i]
               elif board[n_by][n_bx] == 'O' :
                 chk = 0
                 break
-              if board[n_by][n_bx] == '#':
+              elif board[n_by][n_bx] == '#':
                 n_by -= dy[i]
                 break
+            elif 0 == n_by or n_by == N-1 :
+              n_by -= dy[i]
+              break
 
-          if chk == 1 :
-            deq_red.append((n_ry,n_rx))
-            deq_blue.append((n_by,n_bx))
+          if chk == 1 and (n_ry,n_rx,n_by,n_bx) not in checklist :
+            deq.append((n_ry,n_rx,n_by,n_bx))
+            checklist[(n_ry,n_rx,n_by,n_bx)] = 1
 
-          if chk == 2 :
+          if check(n_ry,n_rx,n_by,n_bx) :
             return count
+
   return -1
 
 N, M = map(int,input().split())
@@ -188,9 +232,4 @@ for i in range(1,N) :
       red_y, red_x = i,k
       continue
 
-bfs(red_y,red_x,blue_y,blue_x)
-
-if count <= 10 :
-  print(count)
-else :
-  print(-1)
+print(bfs(red_y,red_x,blue_y,blue_x))
