cd9b81f02dc371fd4b74ceac5d4de7bdd438335b
rbwo <rbwo552@naver.com>
2021-10-07 16:56:23 +0900

    2021_10_07

--- baekjoon/자료구조/컵라면.py
+++ baekjoon/자료구조/컵라면.py
@@ -1,45 +1,20 @@
 import heapq
 
-time = 1
-count = 0
-chk = -1
-
 T = int(input())
 
-ramen = [[] for _ in range(T+2)]
-size = [0 for _ in range(T+2)]
+ramen = []
 
 for i in range(T):
     N, M = map(int,input().split())
-    if len(ramen[N]) == 0 :
-        ramen[N] = [-M]
-    else :
-        ramen[N].append(-M)
+    ramen.append((N,M))
 
-for i in range(1,T+1):
-    size[i] = len(ramen[i])
+ramen.sort()
 
-for i in range(1, T+1):
-    while(time <= i):
-        b = 0
-        if len(ramen[i]) != 0 :
-            heapq.heapify(ramen[i])
-            heapq.heapify(ramen[i+1])
-            a = ramen[i][0]
-            if i != chk and len(ramen[i+1]) != 0:
-                b = ramen[i+1][0]
-            if (a <= b) :
-                count += a
-                heapq.heappop(ramen[i])
-            else :
-                count += b
-                heapq.heappop(ramen[i+1])
+q = []
 
-            time += 1
+for item in ramen :
+    heapq.heappush(q,item[1])
+    if item[0] < len(q) :
+        heapq.heappop(q)
 
-        else :
-            break
-    else :
-        continue
-
-print(-count)
+print(sum(q))
