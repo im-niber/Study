fbc901d9ef8a53ce51de1f242d73b17109116b50
rbwo <rbwo552@naver.com>
2021-10-22 17:16:55 +0900

    2021_10_22

--- baekjoon/정렬/나이순 정렬.py
+++ baekjoon/정렬/나이순 정렬.py
@@ -1,18 +1,36 @@
-n = int(input())
+def my_sol():
+  n = int(input())
 
-name = dict()
+  name = dict()
 
-for i in range(n):
-  age,names = input().split()
+  for i in range(n):
+    age,names = input().split()
 
-  age = int(age)
-  if age not in name :
-    name[age] = [names]
-  else :
-    name[age].append(names)
+    age = int(age)
+    if age not in name :
+      name[age] = [names]
+    else :
+      name[age].append(names)
 
-temp = sorted(name)
+  temp = sorted(name)
 
-for item in temp :
-  for i in range(len(name[item])):
-    print(item, name[item][i])
+  for item in temp :
+    for i in range(len(name[item])):
+      print(item, name[item][i])
+
+#강의 풀이
+def lect_sol():
+  n = int(input())
+  array = []
+
+  for _ in range(n):
+    input_data =input().split(' ')
+    array.append((int(input_data[0]),input_data[1]))
+
+  array = sorted(array, key = lambda x: x[0]) # key 속성으로 정렬해줌.
+
+  for i in array :
+    print(i[0], i[1])
+
+my_sol()
+lect_sol()
