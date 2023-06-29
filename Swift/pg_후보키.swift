0ce36df00fbedeaff7cc0e9c79fa8561f82ee03d
rbwo552 <rbwo552@naver.com>
2023-06-29 19:28:01 +0900

    Update pg_후보키.swift

--- Swift/pg_후보키.swift
+++ Swift/pg_후보키.swift
@@ -1,12 +1,8 @@
 import Foundation
 
-// 데이터가 크지 않아서 반복문을 엄청 돌려서 겨우 해결한 문제 ㅜㅡㅜ
-// 문제 조건에 맞는 idx를 저장해서 겹치는 부분들을 제외하여 count를 세면 답이나온다
-
 func solution(_ relation:[[String]]) -> Int {
     var maxCount = relation[0].count
     var idxSet: Set<Set<Int>> = []
-    var answer = 0
 
     func search(relation: [[String]], idx: Int, item: Set<Int>) {
         if idx == maxCount { return }
@@ -18,12 +14,17 @@ func solution(_ relation:[[String]]) -> Int {
         var duplicateStrs: Set<[String]> = []
         var isUnique = true
         
+        // i 는 배열의 비교값을 얻기 위한 용도
+        // k 는 i에서 나온 값을 남은 배열을 순회하기
+        // j 는 비교할 값의 묶음 ex. 1,2 라면 1번 인덱스, 2번 인덱스가 같이 존재해야
+        // 값을 strs에 대입한다
         for i in 0..<relation.count {
             for k in 0..<relation.count {
                 for j in item {
                     str = relation[i][j]
                     if relation[k][j] != str {
                         isUnique = false
+                        break
                     }
                 }
                 if isUnique { strs.append(relation[k]) }
@@ -40,7 +41,6 @@ func solution(_ relation:[[String]]) -> Int {
         
         if duplicateStrs.count == 0 {
             idxSet.insert(item)
-            answer += 1
             return
         }
         
@@ -54,15 +54,18 @@ func solution(_ relation:[[String]]) -> Int {
     }
     
     var answerSet: Set<Set<Int>> = []
-    
+    // 최소성을 고려안한 집합을 처리하는 부분
+    // 집합요소를 하나 꺼내서 상위 집합이면 해당 인덱스집합은 최소성 규칙에 어긋나므로
+    // flag = false 를 두고 answer에서 제외
     for item in idxSet {
         var flag = true
         for item2 in idxSet {
             if item.isStrictSuperset(of: item2) {
                 flag = false
+                break
             }
         }
         if flag { answerSet.insert(item) }
     }
-    return answer
+    return answerSet.count
 }
