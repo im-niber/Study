2e0706360acde60543ef7e16194044168b01e6b0
rbwo552 <rbwo552@naver.com>
2023-08-13 18:11:50 +0900

    Update pg_튜플.swift

--- Swift/pg_튜플.swift
+++ Swift/pg_튜플.swift
@@ -4,6 +4,36 @@ import Foundation
 // 먼저 [[Int]] 배열로 숫자들을 담아주고, count순으로 정렬을 했다
 // 그리고 set에 넣으면서 기존에 본 숫자인지 체크하고 아니면 정답 배열에 append해서 순서를 지켜줌
 // 다른 사람 풀이의 파싱보면서 좀 더 배워야겠다 다들 괴물이ㅑ~
+
+// 추가로 파싱하는 코드 추가
+func solution(_ s:String) -> [Int] {
+    var result: [Int] = []
+    
+    var sets = s.split(omittingEmptySubsequences: true, whereSeparator: { "}".contains($0) }).map {
+        $0.split(omittingEmptySubsequences: true, whereSeparator: { "{,".contains($0) }).map { Int($0)! }
+    }
+    
+    var sets2 = s.split(separator: "}").map { $0.split(separator: "{").map {
+        $0.split(separator: ",").compactMap { Int($0) }
+    }.flatMap { $0 } }
+    
+    var s = s
+    s.removeFirst(2); s.removeLast(2)
+    var sets3 = s.components(separatedBy: "},{").map {
+        $0.components(separatedBy: ",").compactMap { Int($0) }
+    }
+
+    sets.sort { (lhs, rhs) -> Bool in
+        lhs.count < rhs.count
+    }
+
+    sets.forEach {
+        result.append(Array(Set($0).subtracting(result)).first!)
+    }
+
+    return result
+}
+
 func solution(_ s:String) -> [Int] {
     var numbers: Set<Int> = []
     var res: [Int] = []
