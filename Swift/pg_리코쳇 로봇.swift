95dfc0cc113f6b075850b4d929af6f46cdb17e4d
rbwo552 <rbwo552@naver.com>
2023-09-15 16:47:32 +0900

    Update pg_리코쳇 로봇.swift

--- Swift/pg_리코쳇 로봇.swift
+++ Swift/pg_리코쳇 로봇.swift
@@ -1,5 +1,46 @@
 import Foundation
 
+// pg_리코쳇 로봇 9/15 다시 풀어 볼 일이 있어서 다시푼 코드
+// 기억이 안나서 그런지 이전 풀이랑은 좀 많이 달라진둣
+func solution(_ board:[String]) -> Int {
+    let board = Array(board.map { Array($0) })
+    var res = 1
+    var start = (0, 0)
+    
+    for i in 0..<board.count {
+        for k in 0..<board[0].count {
+            if board[i][k] == "R" { start = (i, k) }
+        }
+    }
+    func verify(_ position: (Int, Int)) -> Bool {
+        (position.0 >= 0 && position.0 < board.count) && (position.1 >= 0 && position.1 < board[0].count)
+    }
+    
+    var queue = [(start, 1)]
+    var visited = Array(repeating: Array(repeating: false, count: board[0].count), count: board.count)
+    let dx = [-1, 1, 0, 0]
+    let dy = [0, 0, 1, -1]
+    
+    while(!queue.isEmpty) {
+        let (temp, count) = queue.removeFirst()
+        
+        var (row, col) = temp
+        visited[row][col] = true
+        
+        for i in 0..<4 {
+            while(verify((row + dy[i], col + dx[i])) && board[row + dy[i]][col + dx[i]] != "D") {
+                row += dy[i]; col += dx[i]
+            }
+        
+            if board[row][col] == "G" { return count }
+            else if board[row][col] == "." && !visited[row][col] { queue.append(((row, col), count + 1)) }
+            (row, col) = temp
+        }
+    }
+
+    return -1
+}
+
 // 프로그래머스 - 리코쳇 로봇
 // 되게 오랜만에 다시 코테 문제 풀어봤는데 겨우겨우 풀었지만 효율이 썩 좋지는 않은듯하다...
 // 다시 열씨미 해야겠그만
