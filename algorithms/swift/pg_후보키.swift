e790dbbdea14524aeb13e24d1c8f6424240e8ff9
rbwo552 <rbwo552@naver.com>
2023-10-03 19:05:22 +0900

    Update pg_후보키.swift

--- algorithms/swift/pg_후보키.swift
+++ algorithms/swift/pg_후보키.swift
@@ -69,3 +69,47 @@ func solution(_ relation:[[String]]) -> Int {
     }
     return answerSet.count
 }
+
+// 새로 풀어본 방식, 스터디에서 다시 풀 일이 생겨서 다시 풀어봄
+// 이번에는 모든 케이스를 배열에 담고 해당 케이스들로 비교를 시작함
+// 예를 들어 이름, 학번의 경우를 비교해야하면 이름과 학번의 String을 더해서
+// Set에 넣고, 마지막으로 해당 count가 relation의 개수만큼 나오는거로 유일성을 판단해씀
+// 마지막에는 가능한 경우들의 집합의 원소를 꺼내서 나머지 원소들의 상위집합인지
+// 비교하여 최소성을 검증하여씀
+func solution(_ relation:[[String]]) -> Int {
+    var maxCount = relation[0].count
+    var cases: [[Int]] = []
+    var possibleCases: Set<Set<Int>> = []
+    
+    func allCase(_ arr: [Int], idx: Int) {
+        if arr.count == maxCount { return }
+        
+        cases.append(arr)
+        for i in idx..<maxCount {
+            allCase(arr + [i], idx: i + 1)
+        }
+    }
+    allCase([], idx: 0)
+    cases.append(Array(0..<maxCount))
+    
+    for `case` in cases {
+        var tempSet: Set<String> = []
+        
+        for row in relation {
+            var tempStr: String = ""
+            for item in `case` {
+                tempStr += row[item]
+            }
+            tempSet.insert(tempStr)
+        }
+        if tempSet.count == relation.count { possibleCases.insert(Set(`case`)) }
+    }
+    var ans = possibleCases.count
+    
+    for item in possibleCases {
+        for item2 in possibleCases {
+            if item.isStrictSuperset(of: item2) { ans -= 1; break }
+        }
+    }
+    return ans
+}
