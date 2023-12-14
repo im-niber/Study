c36f9330f024c96c4a56b5283e082c9609406387
rbwo552 <rbwo552@naver.com>
2023-12-14 17:34:24 +0900

    Update pg_광물캐기.swift

--- algorithms/swift/pg_광물캐기.swift
+++ algorithms/swift/pg_광물캐기.swift
@@ -60,3 +60,65 @@ func solution(_ picks:[Int], _ minerals:[String]) -> Int {
     
     return min(dia, iron, stone)
 }
+
+// 1214 ver
+// 다시 풀일이 있어서 새로 풀었는데
+// 이번엔 재귀로 풀 생각을 하지 못했다 순열 생각은 했어서 그게 그건가
+// 싶기는 한디 여튼 이번엔 그리디로 풀어씀
+// 먼저 곡괭이 개수 만큼 광물들을 나눠주어서 가능한 미네랄 배열을 만들었따
+// 그 다음 피로도가 많이 드는 친구들을 내림차순으로 정렬해서
+// 앞에서부터 다이아, 철, 스톤 괭이로 캐서 답을 냈음
+func solution(_ picks:[Int], _ minerals:[String]) -> Int {
+    var answer = 0
+    
+    var picks = picks
+    let possibleCount = picks.reduce(0, +)
+    var possibleMinerals: [[String]] = []
+    
+    var i = 0
+    var tempArr: [String] = []
+    
+    while(possibleMinerals.count != possibleCount && i < minerals.count) {
+        if tempArr.count == 5 {
+            possibleMinerals.append(tempArr)
+            tempArr = []
+            continue
+        }
+        tempArr.append(minerals[i])
+        i += 1
+    }
+    if !tempArr.isEmpty { possibleMinerals.append(tempArr) }
+    
+    func calValue(arr: [String]) -> Int {
+        return arr.reduce(0) { partialResult, item in
+            if item == "diamond" { return partialResult + 25 }
+            else if item == "iron" { return partialResult + 5 }
+            return partialResult + 1
+        }
+    }
+    let sortedMinerals = possibleMinerals.sorted { lhs, rhs in
+        calValue(arr: lhs) > calValue(arr: rhs)
+    }
+    for item in sortedMinerals {
+        if picks[0] != 0 { answer += item.count; picks[0] -= 1 }
+        else if picks[1] != 0 {
+            for str in item {
+                if str == "diamond" { answer += 5 }
+                else { answer += 1 }
+            }
+            picks[1] -= 1
+        }
+        else {
+            for str in item {
+                if str == "diamond" { answer += 25 }
+                else if str == "iron" { answer += 5 }
+                else { answer += 1 }
+            }
+            picks[2] -= 1
+        }
+        
+        if picks.reduce(0, +) == 0 { break }
+    }
+    
+    return answer
+}
